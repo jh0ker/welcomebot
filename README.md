a87ab725d63684d5c137d0a1ddadffcce3150d74
Jannes Höke <mail@jannes-hoeke.de>
2016-10-24 15:02:31 +0200

    update bot to latest version

--- README.md
+++ README.md
@@ -3,22 +3,13 @@
 
 It uses the [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot) library and [pickledb](https://bitbucket.org/patx/pickledb) for basic persistence.
 
-Please also check out my other bots, [TexBot](https://github.com/jh0ker/texbot) and [GitBot](https://github.com/jh0ker/gitbot).
-
 The file is prepared to be run by anyone by filling out the blanks in the configuration. The bot currently runs on [@jh0ker_welcomebot](https://telegram.me/jh0ker_welcomebot)
 
 ## Required
 * Python 3.4 (may work with earlier versions, untested)
-* [python-telegram-bot](https://github.com/leandrotoledo/python-telegram-bot) module (tested with version 3.1.0)
+* [python-telegram-bot](https://github.com/leandrotoledo/python-telegram-bot) module (tested with version 5.1.1)
 
 ## How to use
 * Clone the repo
-* Edit `BOTNAME` and `TOKEN` in `bot.py`
-* If you want to use webhooks, fill out `BASE_URL`, `HOST` and `PORT` as well
-  * If you also want to handle SSL with python-telegram-bot, fill out the `CERT` and `CERT_KEY` fields and check the section about SSL.
+* Edit `TOKEN` in `bot.py`
 * Follow Bot instructions
-
-## SSL
-You can start the server without an SSL context, if this is handled by another programm, like Apache or haproxy. You can leave out the SSL Information in the header and select the appropriate start line in `main`. 
-
-Please note that you need a valid ssl certificate or a self-signed one. Use `openssl req -new -x509 -nodes -newkey rsa:1024 -keyout server.key -out server.crt -days 3650` to generate your own key and certificate.
--- bot.py
+++ bot.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
 import logging
-
-import sys
-from telegram import Updater, Emoji, ParseMode, TelegramError, Update
-from telegram.dispatcher import run_async
-import python3pickledb as pickledb
+from time import sleep
 import traceback
+import sys
+from html import escape
+
+from telegram import Emoji, ParseMode, TelegramError, Update
+from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
+from telegram.ext.dispatcher import run_async
+from telegram.contrib.botan import Botan
+
+import python3pickledb as pickledb
 
 # Configuration
-BOTNAME = 'examplebot'
+BOTNAME = 'usernameofbot'
 TOKEN = 'TOKEN'
-
-# Fill these if you want to use webhook
-BASE_URL = 'example.com'  # Domain name of your server, without
-# protocol. You may include a port, if you dont want to use 443.
-HOST = '0.0.0.0'  # Public IP Address of your server
-PORT = 5002  # Port on which the Webhook should listen on
-CERT = 'cert.pem'
-CERT_KEY = 'key.key'
 
 help_text = 'Welcomes everyone that enters a group chat that this bot is a ' \
             'part of. By default, only the person who invited the bot into ' \
@@ -25,13 +22,17 @@ help_text = 'Welcomes everyone that enters a group chat that this bot is a ' \
             '/welcome - Set welcome message\n' \
             '/goodbye - Set goodbye message\n' \
             '/disable\\_goodbye - Disable the goodbye message\n' \
-            '/lock - Only the person who invited the bot can change messages\n'\
-            '/unlock - Everyone can change messages\n\n' \
-            'You can use _$username_ and _$title_ as placeholders when setting'\
-            ' messages.\n' \
-            'Please [rate me](http://storebot.me/bot/examplebot) :) ' \
-            'Questions? Message my creator @exampleuser'
-
+            '/lock - Only the person who invited the bot can change messages\n' \
+            '/unlock - Everyone can change messages\n' \
+            '/quiet - Disable "Sorry, only the person who..." ' \
+            '& help messages\n' \
+            '/unquiet - Enable "Sorry, only the person who..." ' \
+            '& help messages\n\n' \
+            'You can use _$username_ and _$title_ as placeholders when setting' \
+            ' messages. [HTML formatting]' \
+            '(https://core.telegram.org/bots/api#formatting-options) ' \
+            'is also supported.\n' \
+            'Please [rate me](http://storebot.me/bot/jh0ker_welcomebot) :) '
 '''
 Create database object
 Database schema:
@@ -51,12 +52,17 @@ if not db.get('chats'):
 
 # Set up logging
 root = logging.getLogger()
-root.setLevel(logging.DEBUG)
+root.setLevel(logging.INFO)
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 logger = logging.getLogger(__name__)
+
+
+@run_async
+def send_async(bot, *args, **kwargs):
+    bot.sendMessage(*args, **kwargs);
 
 
 def check(bot, update, override_lock=None):
@@ -69,8 +75,8 @@ def check(bot, update, override_lock=None):
     chat_str = str(chat_id)
 
     if chat_id > 0:
-        bot.sendMessage(chat_id=chat_id,
-                        text='Please add me to a group first!')
+        send_async(bot, chat_id=chat_id,
+                   text='Please add me to a group first!')
         return False
 
     locked = override_lock if override_lock is not None \
@@ -78,7 +84,7 @@ def check(bot, update, override_lock=None):
 
     if locked and db.get(chat_str + '_adm') != update.message.from_user.id:
         if not db.get(chat_str + '_quiet'):
-            bot.sendMessage(chat_id=chat_id, text='Sorry, only the person who '
+            send_async(bot, chat_id=chat_id, text='Sorry, only the person who '
                                                   'invited me can do that.')
         return False
 
@@ -91,10 +97,10 @@ def welcome(bot, update):
 
     message = update.message
     chat_id = message.chat.id
-    logger.debug('%s joined to chat %d (%s)'
-                 % (message.new_chat_participant.first_name,
+    logger.info('%s joined to chat %d (%s)'
+                 % (escape(message.new_chat_member.first_name),
                     chat_id,
-                    message.chat.title))
+                    escape(message.chat.title)))
 
     # Pull the custom message for this chat from the database
     text = db.get(str(chat_id))
@@ -106,9 +112,9 @@ def welcome(bot, update):
 
     # Replace placeholders and send message
     text = text.replace('$username',
-                        message.new_chat_participant.first_name)\
+                        message.new_chat_member.first_name)\
         .replace('$title', message.chat.title)
-    bot.sendMessage(chat_id=chat_id, text=text)
+    send_async(bot, chat_id=chat_id, text=text, parse_mode=ParseMode.HTML)
 
 
 # Welcome a user to the chat
@@ -117,10 +123,10 @@ def goodbye(bot, update):
 
     message = update.message
     chat_id = message.chat.id
-    logger.debug('%s left chat %d (%s)'
-                 % (message.left_chat_participant.first_name,
+    logger.info('%s left chat %d (%s)'
+                 % (escape(message.left_chat_member.first_name),
                     chat_id,
-                    message.chat.title))
+                    escape(message.chat.title)))
 
     # Pull the custom message for this chat from the database
     text = db.get(str(chat_id) + '_bye')
@@ -135,9 +141,9 @@ def goodbye(bot, update):
 
     # Replace placeholders and send message
     text = text.replace('$username',
-                        message.left_chat_participant.first_name)\
+                        message.left_chat_member.first_name)\
         .replace('$title', message.chat.title)
-    bot.sendMessage(chat_id=chat_id, text=text)
+    send_async(bot, chat_id=chat_id, text=text, parse_mode=ParseMode.HTML)
 
 
 # Introduce the bot to a chat its been added to
@@ -160,7 +166,7 @@ def introduce(bot, update):
            ' nice message %s \nCheck the /help command for more info!'\
            % (update.message.chat.title,
               Emoji.GRINNING_FACE_WITH_SMILING_EYES)
-    bot.sendMessage(chat_id=chat_id, text=text)
+    send_async(bot, chat_id=chat_id, text=text)
 
 
 # Print help text
@@ -168,11 +174,13 @@ def help(bot, update):
     """ Prints help text """
 
     chat_id = update.message.chat.id
-
-    bot.sendMessage(chat_id=chat_id,
-                    text=help_text,
-                    parse_mode=ParseMode.MARKDOWN,
-                    disable_web_page_preview=True)
+    chat_str = str(chat_id)
+    if (not db.get(chat_str + '_quiet') or db.get(chat_str + '_adm') ==
+            update.message.from_user.id):
+        send_async(bot, chat_id=chat_id,
+                   text=help_text,
+                   parse_mode=ParseMode.MARKDOWN,
+                   disable_web_page_preview=True)
 
 
 # Set custom message
@@ -190,16 +198,17 @@ def set_welcome(bot, update, args):
 
     # Only continue if there's a message
     if not message:
-        bot.sendMessage(chat_id=chat_id, text='You need to send a message,'
-                                              ' too! For example:\n'
-                                              '/welcome Hello $username,'
-                                              ' welcome to $title!')
+        send_async(bot, chat_id=chat_id, 
+                   text='You need to send a message, too! For example:\n'
+                        '<code>/welcome Hello $username, welcome to '
+                        '$title!</code>',
+                   parse_mode=ParseMode.HTML)
         return
 
     # Put message into database
     db.set(str(chat_id), message)
 
-    bot.sendMessage(chat_id=chat_id, text='Got it!')
+    send_async(bot, chat_id=chat_id, text='Got it!')
 
 
 # Set custom message
@@ -217,16 +226,16 @@ def set_goodbye(bot, update, args):
 
     # Only continue if there's a message
     if not message:
-        bot.sendMessage(chat_id=chat_id, text='You need to send a message,'
-                                              ' too! For example:\n'
-                                              '/goodbye Goodbye, '
-                                              '$username!')
+        send_async(bot, chat_id=chat_id, 
+                   text='You need to send a message, too! For example:\n'
+                        '<code>/goodbye Goodbye, $username!</code>',
+                   parse_mode=ParseMode.HTML)
         return
 
     # Put message into database
     db.set(str(chat_id) + '_bye', message)
 
-    bot.sendMessage(chat_id=chat_id, text='Got it!')
+    send_async(bot, chat_id=chat_id, text='Got it!')
 
 
 def disable_goodbye(bot, update):
@@ -241,7 +250,7 @@ def disable_goodbye(bot, update):
     # Disable goodbye message
     db.set(str(chat_id) + '_bye', False)
 
-    bot.sendMessage(chat_id=chat_id, text='Got it!')
+    send_async(bot, chat_id=chat_id, text='Got it!')
 
 
 def lock(bot, update):
@@ -256,7 +265,7 @@ def lock(bot, update):
     # Lock the bot for this chat
     db.set(str(chat_id) + '_lck', True)
 
-    bot.sendMessage(chat_id=chat_id, text='Got it!')
+    send_async(bot, chat_id=chat_id, text='Got it!')
 
 
 def quiet(bot, update):
@@ -271,7 +280,7 @@ def quiet(bot, update):
     # Lock the bot for this chat
     db.set(str(chat_id) + '_quiet', True)
 
-    bot.sendMessage(chat_id=chat_id, text='Got it!')
+    send_async(bot, chat_id=chat_id, text='Got it!')
 
 
 def unquiet(bot, update):
@@ -286,7 +295,7 @@ def unquiet(bot, update):
     # Lock the bot for this chat
     db.set(str(chat_id) + '_quiet', False)
 
-    bot.sendMessage(chat_id=chat_id, text='Got it!')
+    send_async(bot, chat_id=chat_id, text='Got it!')
 
 
 def unlock(bot, update):
@@ -301,7 +310,7 @@ def unlock(bot, update):
     # Unlock the bot for this chat
     db.set(str(chat_id) + '_lck', False)
 
-    bot.sendMessage(chat_id=chat_id, text='Got it!')
+    send_async(bot, chat_id=chat_id, text='Got it!')
 
 
 def empty_message(bot, update):
@@ -318,78 +327,19 @@ def empty_message(bot, update):
         db.set('chats', chats)
         logger.info("I have been added to %d chats" % len(chats))
 
-    if update.message.new_chat_participant is not None:
+    if update.message.new_chat_member is not None:
         # Bot was added to a group chat
-        if update.message.new_chat_participant.username == BOTNAME:
+        if update.message.new_chat_member.username == BOTNAME:
             return introduce(bot, update)
         # Another user joined the chat
         else:
             return welcome(bot, update)
 
     # Someone left the chat
-    elif update.message.left_chat_participant is not None:
-        if update.message.left_chat_participant.username != BOTNAME:
+    elif update.message.left_chat_member is not None:
+        if update.message.left_chat_member.username != BOTNAME:
             return goodbye(bot, update)
 
-
-def broadcast(bot, update, args):
-    """
-    CLI command handler to send a broadcast message to all entries in the chat
-    list. Used to send information about updates. Deleted or blocked chats will
-    be deleted.
-    """
-
-    chats = db.get('chats')
-    text = ' '.join(args)
-
-    for chat_id in chats:
-        print("Messaging chat %d" % chat_id)
-        try:
-            bot.sendMessage(chat_id=chat_id, text=text)
-        except TelegramError as te:
-            logger.warn(te)
-            chats.remove(chat_id)
-            logger.info("Removed chat_id %s from chat list." % chat_id)
-
-        except:
-            logger.warn("Error on chat_id %d:" % chat_id)
-            traceback.print_exc()
-
-    if len(chats) > 25:
-        db.set('chats', chats)
-        print("Broadcasted message!")
-    else:
-        print("Not deleted chat list - something seems to be wrong!")
-
-
-def set_log_level(bot, update, args):
-    """ Another CLI command. Changes the logging level for the console. """
-
-    level = args[0]
-
-    if level == "DEBUG":
-        level = logging.DEBUG
-    elif level == "INFO":
-        level = logging.INFO
-    elif level == "WARNING":
-        level = logging.WARNING
-    elif level == "ERROR":
-        level = logging.ERROR
-    else:
-        logger.error("Unkown logging level.")
-        return
-
-    logging.basicConfig(level=level,
-                        format='%(asctime)s - %(name)s - '
-                               '%(levelname)s - %(message)s')
-    logger.log(level, "Set logging level!")
-
-
-def chatcount(bot, update):
-    """ CLI command to print the amount of groups we're in """
-
-    chats = db.get('chats')
-    print("Added to %s groups." % len([chat for chat in chats if chat < 0]))
 
 
 def error(bot, update, error, **kwargs):
@@ -413,56 +363,42 @@ def error(bot, update, error, **kwargs):
         pass
 
 
+botan = Botan('XZY-HRoWfG_AJEimJi47Ku0bWwJDCbAC')
+
+
+@run_async
+def stats(bot, update, **kwargs):
+    if botan.track(update.message):
+        logger.debug("Tracking with botan.io successful")
+    else:
+        logger.info("Tracking with botan.io failed")
+
+
 def main():
     # Create the Updater and pass it your bot's token.
-    updater = Updater(TOKEN, workers=2)
+    updater = Updater(TOKEN, workers=10)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    dp.addTelegramCommandHandler("start", help)
-    dp.addTelegramCommandHandler("help", help)
-    dp.addTelegramCommandHandler('welcome', set_welcome)
-    dp.addTelegramCommandHandler('goodbye', set_goodbye)
-    dp.addTelegramCommandHandler('disable_goodbye', disable_goodbye)
-    dp.addTelegramCommandHandler("lock", lock)
-    dp.addTelegramCommandHandler("unlock", unlock)
-    dp.addTelegramCommandHandler("quiet", quiet)
-    dp.addTelegramCommandHandler("unquiet", unquiet)
+    dp.add_handler(CommandHandler("start", help))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler('welcome', set_welcome, pass_args=True))
+    dp.add_handler(CommandHandler('goodbye', set_goodbye, pass_args=True))
+    dp.add_handler(CommandHandler('disable_goodbye', disable_goodbye))
+    dp.add_handler(CommandHandler("lock", lock))
+    dp.add_handler(CommandHandler("unlock", unlock))
+    dp.add_handler(CommandHandler("quiet", quiet))
+    dp.add_handler(CommandHandler("unquiet", unquiet))
 
-    dp.addTelegramRegexHandler('^$', empty_message)
+    dp.add_handler(MessageHandler([Filters.status_update], empty_message))
+    dp.add_handler(MessageHandler([Filters.text], stats))
 
-    dp.addStringCommandHandler('broadcast', broadcast)
-    dp.addStringCommandHandler('level', set_log_level)
-    dp.addStringCommandHandler('count', chatcount)
+    dp.add_error_handler(error)
 
-    dp.addErrorHandler(error)
+    update_queue = updater.start_polling(timeout=30, clean=False)
 
-    # Start the Bot and store the update Queue, so we can insert updates
-    update_queue = updater.start_polling(poll_interval=1, timeout=5)
-
-    '''
-    # Alternatively, run with webhook:
-    updater.bot.setWebhook(webhook_url='https://%s/%s' % (BASE_URL, TOKEN))
-
-    # Or, if SSL is handled by a reverse proxy, the webhook URL is already set
-    # and the reverse proxy is configured to deliver directly to port 6000:
-
-    update_queue = updater.start_webhook(HOST, PORT, url_path=TOKEN)
-    '''
-
-    # Start CLI-Loop
-    while True:
-        text = input()
-
-        # Gracefully stop the event handler
-        if text == 'stop':
-            updater.stop()
-            break
-
-        # else, put the text into the update queue
-        elif len(text) > 0:
-            update_queue.put(text)  # Put command into queue
+    updater.idle()
 
 if __name__ == '__main__':
     main()
