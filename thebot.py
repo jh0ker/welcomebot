d72eb0f6266d58adf4087fab87bffbd8b8a6113b
FeoRache <feorache@protonmail.com>
2019-07-18 21:45:27 +0300

    Update, remove /image
    Signed-off-by: FeoRache <feorache@protonmail.com>

--- thebot.py
+++ thebot.py
@@ -42,6 +42,10 @@ ERROR_DELAY = 1
 # Bot ID
 BOT_ID = 705781870
 
+# Request timeout time in seconds
+REQUEST_TIMEOUT = 1.2
+
+
 def help(update, context):
     """Help message"""
     if antispammer_check_passed(update):
@@ -51,13 +55,12 @@ def help(update, context):
             "/help - Это меню;\n"
             "/cat - Случайное фото котика;\n"
             "/dog - Случайное фото собачки;\n"
-            "/image [тематика] - Случайное фото. Можно задать тематику на английском;\n"
             "/dadjoke - Случайная шутка бати;\n"
             "/slap - Кого-то унизить (надо ответить жертве, чтобы бот понял кого бить);\n"
             "\n"
             "Генераторы чисел:\n"
-            "/myiq - Мой IQ (0 - 200);\n"
-            "/muhdick - Длина моего шланга (0 - 25);\n"
+            "/myiq - Мой IQ (1 - 200);\n"
+            "/muhdick - Длина моего шланга (1 - 25);\n"
             "/flip - Бросить монетку (Орёл или Решка);\n"
             "\n"
             "Дополнительная информация:\n"
@@ -68,9 +71,7 @@ def help(update, context):
             f"4. Ошибка о кулдауне даётся минимум через каждую {ERROR_DELAY} минуту. "
             f"Спам удаляется.\n"
         )
-        bot.send_message(chat_id=update.message.chat_id,
-                         text=help_text,
-                         reply_to_message_id=update.message.message_id)
+        _send_message(update, help_text)
 
 
 def welcomer(update, context):
@@ -90,19 +91,15 @@ def welcomer(update, context):
         new_member = update.message.new_chat_members[0].first_name
         reply_text = (f"Приветствуем вас в Думерском Чате, {new_member}!\n"
                       f"По традициям группы, с вас фото своих ног.\n")
-    bot.send_message(chat_id=update.message.chat_id,
-                     text=reply_text,
-                     reply_to_message_id=update.message.message_id)
+    _send_message(update, reply_text)
 
 
 def farewell(update, context):
     """Goodbye message"""
     # A a bot was removed
     if update.message.left_chat_member.is_bot:
-        bot.send_message(chat_id=update.message.chat_id,
-                         text=f"{update.message.left_chat_member.first_name}'a убили, красиво. "
-                         f"Уважаю.",
-                         reply_to_message_id=update.message.message_id)
+        _send_message(
+            update, f"{update.message.left_chat_member.first_name}'a убили, красиво. Уважаю.")
 
 
 def reply_to_text(update, context):
@@ -125,7 +122,8 @@ def reply_to_text(update, context):
         if doomer_word_start is not None:
             # Find the word in the message, get the word and all adjacent symbol
             word_with_symbols = \
-                update.message.text.lower()[doomer_word_start:].replace(found_word, 'хуюмер').split()[0]
+                update.message.text.lower()[doomer_word_start:].replace(
+                    found_word, 'хуюмер').split()[0]
             reply_word = ''
             # Get only the word, until any number or non alpha symbol is encountered
             for i in word_with_symbols:
@@ -134,52 +132,41 @@ def reply_to_text(update, context):
                 else:
                     break
             # Send reply
-            bot.send_message(chat_id=update.message.chat_id,
-                             text=reply_word,
-                             reply_to_message_id=update.message.message_id)
+            _send_message(update, reply_word)
 
 
 def flip(update, context):
     """Flip a Coin"""
     if antispammer_check_passed(update):
-        flip_outcome = random.choice(['Орёл!', 'Решка!'])
-        bot.send_message(chat_id=update.message.chat_id,
-                         text=flip_outcome,
-                         reply_to_message_id=update.message.message_id)
+        _send_message(update, random.choice(['Орёл!', 'Решка!']))
 
 
 def myiq(update, context):
-    """Return IQ level (0-200)"""
+    """Return IQ level (1 - 200)"""
     if antispammer_check_passed(update):
-        iq_level = random.randint(0, 200)
+        iq_level = random.randint(1, 200)
+        reply_text = f"Твой уровень IQ {iq_level}. "
         if iq_level < 85:
-            message = f"Твой уровень IQ {iq_level}. Грустно за тебя, братишка. (0 - 200)"
+            reply_text += "Грустно за тебя, братишка. (1 - 200)"
         elif 85 <= iq_level <= 115:
-            message = f"Твой уровень IQ {iq_level}. Ты норми, братишка. (0 - 200)"
+            reply_text += "Ты норми, братишка. (1 - 200)"
         elif 115 < iq_level <= 125:
-            message = f"Твой уровень IQ {iq_level}. Ты умный, братишка! (0 - 200)"
+            reply_text += "Ты умный, братишка! (1 - 200)"
         else:
-            message = f"Твой уровень IQ {iq_level}. Ты гений, братишка! (0 - 200)"
-        bot.send_message(chat_id=update.message.chat_id,
-                         text=message,
-                         reply_to_message_id=update.message.message_id)
+            reply_text += "Ты гений, братишка! (1 - 200)"
+        _send_message(update, reply_text)
 
 
 def muhdick(update, context):
-    """Return dick size in cm (0-25)"""
+    """Return dick size in cm (1 - 25)"""
     if antispammer_check_passed(update):
-        muh_dick = random.randint(0, 25)
-        if muh_dick == 0:
-            reply_text = f"У тебя нет члена (0 см) \U0001F62C! Ты евнух, братишка. (0 - 25)"
-        elif 1 <= muh_dick <= 11:
-            reply_text = f"Длина твоего стручка {muh_dick} см \U0001F923! (0 - 25)"
-        elif 12 <= muh_dick <= 17:
-            reply_text = f"Длина твоей палочки {muh_dick} см! (0 - 25)"
-        else:
-            reply_text = f"Длина твоего шланга {muh_dick} см! (0 - 25)"
-        bot.send_message(chat_id=update.message.chat_id,
-                         text=reply_text,
-                         reply_to_message_id=update.message.message_id)
+        muh_dick = random.randint(1, 25)
+        reply_text = f"Длина твоей палочки {muh_dick} см! "
+        if 1 <= muh_dick <= 11:
+            reply_text += "\U0001F92D (1 - 25)"
+        elif 21 <= muh_dick <= 25:
+            reply_text += "\U0001F631 (1 - 25)"
+        _send_message(update, reply_text)
 
 
 def dog(update, context):
@@ -188,12 +175,16 @@ def dog(update, context):
     # let the server download the image/video/gif
     if antispammer_check_passed(update):
         try:
-            response = requests.get('https://random.dog/woof.json', timeout=1).json()
-            if 'mp4' in response['url'].split('.')[-1]:
+            response = requests.get(
+                'https://random.dog/woof.json', timeout=REQUEST_TIMEOUT).json()
+            # Get the file extension of the file
+            file_extension = response['url'].split('.')[-1]
+            # Depending on the file extension, use the proper bot method
+            if 'mp4' in file_extension:
                 bot.send_video(chat_id=update.message.chat_id,
                                video=response['url'],
                                reply_to_message_id=update.message.message_id)
-            elif 'gif' in response['url'].split('.')[-1]:
+            elif 'gif' in file_extension:
                 bot.send_animation(chat_id=update.message.chat_id,
                                    animation=response['url'],
                                    reply_to_message_id=update.message.message_id)
@@ -202,7 +193,8 @@ def dog(update, context):
                                photo=response['url'],
                                reply_to_message_id=update.message.message_id)
         except requests.exceptions.ReadTimeout:
-            _time_out_response(update)
+            _send_message(update, 'Думер умер на пути к серверу.')
+
 
 def cat(update, context):
     """Get a random cat image"""
@@ -210,68 +202,13 @@ def cat(update, context):
     # let the server download the image
     if antispammer_check_passed(update):
         try:
-            response = requests.get('http://aws.random.cat/meow', timeout=1).json()
+            response = requests.get(
+                'http://aws.random.cat/meow', timeout=REQUEST_TIMEOUT).json()
             bot.send_photo(chat_id=update.message.chat_id,
                            photo=response['file'],
                            reply_to_message_id=update.message.message_id)
         except requests.exceptions.ReadTimeout:
-            _time_out_response(update)
-
-
-def image(update, context):
-    """Create a command for random images"""
-
-    def image_unsplash(user_theme, update):
-        """Function that gets random images from unsplash.com"""
-        # Create a user request
-        user_theme = ','.join(user_theme)
-        # Ask the server
-        try:
-            response = requests.get(
-                f'https://source.unsplash.com/500x700/?{user_theme}', timeout=1)
-            # Reply the response with the photo
-            bot.send_photo(chat_id=update.message.chat_id,
-                           photo=response.url,
-                           reply_to_message_id=update.message.message_id)
-        except requests.exceptions.ReadTimeout:
-            _time_out_response(update)
-
-    def image_pixabay(user_theme, update):
-        """Function that gets random images from pixabay.com"""
-        # Create a user request
-        user_theme = '+'.join(user_theme)
-        # Request the server for the dictionary with links to images
-        try:
-            response = requests.get('https://pixabay.com/api/',
-                                    params={
-                                        'key': '12793256-08bafec09c832951d5d3366f1',
-                                        'q': user_theme,
-                                        "safesearch": "false",
-                                        "lang": "en"
-                                    }, timeout=1).json()
-            # If there are hits, reply with photo
-            if response['totalHits'] != 0:
-                photo = random.choice(response['hits'])['largeImageURL']
-                bot.send_photo(chat_id=update.message.chat_id,
-                               photo=photo,
-                               reply_to_message_id=update.message.message_id)
-            # If no hits, give an error
-            else:
-                bot.send_message(chat_id=update.message.chat_id,
-                                 text='Фото по запросу не найдено.',
-                                 reply_to_message_id=update.message.message_id)
-        except requests.exceptions.ReadTimeout:
-            _time_out_response(update)
-
-    if antispammer_check_passed(update):
-        # Get user request, remove the bot command
-        user_request = update.message.text.split()
-        user_request.pop(0)
-        # Use a random source
-        if random.randint(0, 1) == 0:
-            image_pixabay(user_request, update)
-        else:
-            image_unsplash(user_request, update)
+            _send_message(update, 'Думер умер на пути к серверу.')
 
 
 def dadjoke(update, context):
@@ -283,12 +220,11 @@ def dadjoke(update, context):
         }
         try:
             response = requests.get(
-                'https://icanhazdadjoke.com/', headers=headers).json()
-            bot.send_message(chat_id=update.message.chat_id,
-                             reply_to_message_id=update.message.message_id,
-                             text=response['joke'])
+                'https://icanhazdadjoke.com/', headers=headers, timeout=REQUEST_TIMEOUT).json()
+            _send_message(update, response['joke'])
         except requests.exceptions.ReadTimeout:
-            _time_out_response(update)
+            _send_message(update, 'Думер умер на пути к серверу.')
+
 
 def slap(update, context):
     """Slap with random item"""
@@ -314,10 +250,7 @@ def slap(update, context):
             reply_text = f"[{update.message.from_user.first_name}](tg://user?id={update.message.from_user.id}) {action} " \
                 f"[{update.message.reply_to_message.from_user.first_name}](tg://user?id={update.message.reply_to_message.from_user.id}) " \
                 f"{random.choice(action_items[action])}."
-        bot.send_message(chat_id=update.message.chat_id,
-                         reply_to_message_id=update.message.message_id,
-                         text=reply_text,
-                         parse_mode='Markdown')
+        _send_message(update, reply_text, parse_mode='Markdown')
 
 
 def rules(update, context):
@@ -327,9 +260,7 @@ def rules(update, context):
                   "3. Никаких гей-гифок;\n"
                   "4. За спам - ноги;\n"
                   "5. Думерскую историю рассказать;\n")
-    bot.send_message(chat_id=update.message.chat_id,
-                     reply_to_message_id=update.message.message_id,
-                     text=reply_text)
+    _send_message(update, reply_text)
 
 
 def antispammer_check_passed(update):
@@ -389,17 +320,15 @@ def antispammer_check_passed(update):
         if message_time > (SPAM_COUNTER[update.message.chat_id]['last_error']
                            + datetime.timedelta(minutes=ERROR_DELAY)):
             SPAM_COUNTER[update.message.chat_id]['last_error'] = message_time
-            bot.send_message(chat_id=update.message.chat_id,
-                             reply_to_message_id=update.message.message_id,
-                             text=error_message + f"Эта ошибка тоже появляется минимум каждую {ERROR_DELAY} минуту.\n"
-                             f"Запросы во время кулдауна ошибки будут удаляться.")
+            error_message += (f"Эта ошибка тоже появляется минимум каждую {ERROR_DELAY} минуту.\n"
+                              f"Запросы во время кулдауна ошибки будут удаляться.")
+            _send_message(update, error_message)
         else:
             _try_to_delete_message(update)
     else:
-        bot.send_message(chat_id=update.message.chat_id,
-                         reply_to_message_id=update.message.message_id,
-                         text=error_message + f"Эта ошибка тоже появляется минимум каждую {ERROR_DELAY} минуту.\n"
-                         f"Запросы во время кулдауна ошибки будут удаляться.")
+        error_message += (f"Эта ошибка тоже появляется минимум каждую {ERROR_DELAY} минуту.\n"
+                          f"Запросы во время кулдауна ошибки будут удаляться.")
+        _send_message(update, error_message)
         SPAM_COUNTER[update.message.chat_id]['last_error'] = message_time
     return False
 
@@ -413,11 +342,12 @@ def _try_to_delete_message(update):
         pass
 
 
-def _time_out_response(update):
-    """Response reserved for cases when the bot can't reach the server for request."""
+def _send_message(update, text: str, parse_mode: str = None):
+    """Shortener of replies"""
     bot.send_message(chat_id=update.message.chat_id,
-                     text='Думер умер на пути к серверу.',
-                     reply_to_message_id=update.message.message_id)
+                     text=text,
+                     reply_to_message_id=update.message.message_id,
+                     parse_mode=parse_mode)
 
 
 def error(update, context):
@@ -440,7 +370,6 @@ def main():
     dispatcher.add_handler(CommandHandler("muhdick", muhdick))
     dispatcher.add_handler(CommandHandler("dog", dog))
     dispatcher.add_handler(CommandHandler("cat", cat))
-    dispatcher.add_handler(CommandHandler("image", image))
     dispatcher.add_handler(CommandHandler("dadjoke", dadjoke))
     dispatcher.add_handler(CommandHandler("slap", slap))
     dispatcher.add_handler(CommandHandler("rules", rules))
