1bcc66076885cae3f175d7de5bcbb1a826ccf8fd
FeoRache <feorache@protonmail.com>
2020-01-21 12:20:59 +0100

    Update
    Signed-off-by: FeoRache <feorache@protonmail.com>

--- maindoomer/maincommands/duelranking.py
+++ maindoomer/maincommands/duelranking.py
@@ -3,6 +3,7 @@
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
+from constants import DUELDICT as DD
 from maindoomer import BOT
 from maindoomer.helpers import check_if_group_chat, command_antispam_passed
 from maindoomer.sqlcommands import run_query
@@ -27,7 +28,7 @@ def duelranking(update: Update, context: CallbackContext) -> None:
 @run_async
 @command_antispam_passed
 def _handle_ranking(update: Update) -> None:
-    header = '***Убийства/Смерти/Промахи***\n'
+    header = '***Убийства/Смерти/Промахи/Модификатор силы***\n'
     ranking = ''
     # Get top 10 from the duel table
     chat_data = enumerate(run_query(f'''SELECT
@@ -44,12 +45,16 @@ def _handle_ranking(update: Update) -> None:
         ranking = '\nПока что недостаточно данных. Продолжайте дуэлиться.'
     else:
         for Q in chat_data:
+            wr_increase = Q[1][1] * DD['KILLMULTPERC'] + \
+                Q[1][2] * DD['DEATHMULTPERC'] + \
+                Q[1][3] * DD['MISSMULTPERC']
+            wr_increase = min(round(wr_increase, 2), 45)
             ranking += (f'№{Q[0]+1} {Q[1][0]}\t -\t '
-                        f'{Q[1][1]}/{Q[1][2]}/{Q[1][3]}\t -\t '
-                        f'{Q[1][4]} очков\n')
+                        f'{Q[1][1]}/{Q[1][2]}/{Q[1][3]}/{wr_increase}\n')
     BOT.send_message(
         chat_id=update.effective_chat.id,
         reply_to_message_id=update.effective_message.message_id,
-        text=header + ranking + 'Показывается топ 10',
+        text=header + ranking +
+        'Показывается топ 10 по очкам. 3/2/1 очко за убийство/смерть/промах.',
         parse_mode='Markdown'
     )
